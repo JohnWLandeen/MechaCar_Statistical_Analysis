a9109ba4dbcde28b66126164d5084fde61687ceb
JohnWLandeen <93979931+JohnWLandeen@users.noreply.github.com>
2022-09-23 13:56:36 -0700

    changed dependant var to mpg

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,6 +3,6 @@ library(dplyr)
 #Read in MechaCar data
 M_car <- read.csv('data/MechaCar_mpg.csv')
 #Perform linear regression
-lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=M_car)
+lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length,data=M_car)
 #Summarize the linear regression
-summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=M_car))
+summary(lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length,data=M_car))
